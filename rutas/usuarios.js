5999a6056960e4578556be90e20e92d94a6e1328
Marck2004 <marcosmaestrocobo@gmail.com>
2024-02-26 16:35:58 +0100

    ok

--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -7,5 +7,6 @@ router.put("/modifUsuario",controller.modifUsuario);
 router.get("/usuarios",controller.devolverUsuarios);
 router.post("/añadirUsuario",controller.addUsuario);
 router.delete("/usuarios/:nombre",controller.removeUsuario);
+router.post("/addUsuario",controller.addUsuario);
 
 module.exports = router;
